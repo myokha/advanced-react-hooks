d6a059803d994845dc6a56660ef4307e0831b97b
Max Yokha <max.yokha@gmail.com>
2021-10-04 23:42:46 +0300

    01.extra-2: simulate setState with an object

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,20 +3,22 @@
 
 import * as React from 'react'
 
-function countReducer(count, step) {
-  return step + step
+function countReducer(state, action) {
+  return { ...state, ...action }
 }
 
 function Counter({initialCount = 0, step = 1}) {
   // 🐨 replace React.useState with React.useReducer.
   // 💰 React.useReducer(countReducer, initialCount)
-  const [count, changeCount] = React.useReducer(countReducer, initialCount)
+  const [state, setState] = React.useReducer(countReducer, { count: initialCount })
+
+  const {count} = state
 
   // 💰 you can write the countReducer function so you don't have to make any
   // changes to the next two lines of code! Remember:
   // The 1st argument is called "state" - the current value of count
   // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => changeCount(step)
+  const increment = () => setState({count: count + step})
   return <button onClick={increment}>{count}</button>
 }
 
